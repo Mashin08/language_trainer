95c0603795c9fa93b05d0a6bc7950b1cf3e5db96
Mashin08 <mashin.na@phystech.edu>
2025-04-13 17:31:50 +0300

    Fixed secret issue

--- language_trainer/settings.py
+++ language_trainer/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6!-nk=0znkg-lyoecm3h@-o9ryzfo%($%@$f$+kw@vu*aldx!_'
+dotenv_path = os.path.join(BASE_DIR, ".env")
+load_dotenv()
+
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
